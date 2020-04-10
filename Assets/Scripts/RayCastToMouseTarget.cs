0018e0e83d6c7886251c905812bd8d2e2b178c08
Ryon <ryon.everett@gmail.com>
2020-04-10 16:02:21 -0400

    added raycast hit

---
+++ Assets/Scripts/RayCastToMouseTarget.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RayCastToMouseTarget : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	if (Input.GetKey(KeyCode.Mouse0))
+	{
+		CastRay();
+	}
+    }
+
+    void CastRay()
+    {
+        RaycastHit hit;
+        Ray ray = GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+       
+        if (Physics.Raycast(ray, out hit))
+        {
+            Transform objectHit = hit.transform;
+            Debug.Log("Ray direction is " + ray.direction);
+            Debug.DrawRay(ray.origin, (ray.direction * hit.distance) , Color.green, 3f, true);
+            Debug.Log("I am shooting a thing at " + hit.collider.gameObject + " at " + ray.GetPoint(objectHit.position.x));
+            // Do something with the object that was hit by the raycast.
+        }
+    }
+}
